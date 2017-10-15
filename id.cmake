f454ed73fc73746c55af472316d9257d6e669f52
David Callu <callu.david@gmail.com>
2017-10-15 19:29:43 +0200

    feat(cmake): add data to build version 1.0.6

---
+++ id.cmake
@@ -0,0 +1,14 @@
+set(package bzip2)
+
+byd__package__info(${package}
+    MAINTAINER_NAME "David Callu"
+    MAINTAINER_EMAIL "callu.david@gmail.com"
+    VERSION 1.0.6
+    PACKAGE_VERSION 0.1
+    ABI 1.0
+    )
+
+byd__package__download_info(
+    URL "https://github.com/hunter-packages/bzip2/archive/v1.0.6-p1.tar.gz"
+    URL_HASH SHA1=5eae50a9a0ded0ee0ea5201001b2f4f726dbf8ed
+    )
