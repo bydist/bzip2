0b04878d5a1ba427dc47b573f5beffa1b2f2ab73
David Callu <callu.david@gmail.com>
2017-10-16 09:17:22 +0200

    fix(id): add missing argument to byd__package__download_info

--- id.cmake
+++ id.cmake
@@ -8,7 +8,7 @@ byd__package__info(${package}
     ABI 1.0
     )
 
-byd__package__download_info(
+byd__package__download_info(${package}
     URL "https://github.com/hunter-packages/bzip2/archive/v1.0.6-p1.tar.gz"
     URL_HASH SHA1=5eae50a9a0ded0ee0ea5201001b2f4f726dbf8ed
     )
