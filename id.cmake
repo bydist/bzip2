30c8aaec0a7f886ad8628c8554c01c3c21ae5b87
David Callu <callu.david@gmail.com>
2018-09-05 15:42:42 +0200

    feat(id): bump version to 1.0.6-4
    - use bzip 1.0.6-p4 that remove C++ comment from C header file

--- id.cmake
+++ id.cmake
@@ -4,11 +4,11 @@ set(version 1.0.6)
 byd__package__info(${package}
     MAINTAINER_NAME "David Callu"
     MAINTAINER_EMAIL "callu.david@gmail.com"
-    VERSION ${version}-3
+    VERSION ${version}-4
     ABI 1.0
     )
 
 byd__package__download_info(${package}
-    URL "https://github.com/hunter-packages/bzip2/archive/v1.0.6-p1.tar.gz"
-    URL_HASH SHA1=5eae50a9a0ded0ee0ea5201001b2f4f726dbf8ed
+    URL "https://github.com/ledocc/bzip2/archive/v1.0.6-p4.tar.gz"
+    URL_HASH SHA1=5e64be7fed4061b3590b89a1896b9a3bf6ac5d1a
     )
